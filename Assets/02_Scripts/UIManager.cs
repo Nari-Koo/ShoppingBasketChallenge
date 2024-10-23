bbad64fe7a26bf5b844285efe83784216afca610
KooNaRi <n.hodu210@gmail.com>
2024-10-23 14:52:48 +0900

    버튼 연결 및 씬 변경

---
+++ Assets/02_Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private Button level1Button;
+    [SerializeField] private Button level2Button;
+    [SerializeField] private Button level3Button;
+    [SerializeField] private Button exitButton;
+
+    void Start()
+    {
+        level1Button.onClick.AddListener(() => onLevel1ButtonClick());
+        exitButton.onClick.AddListener(() => onExitButtonClick());
+    }
+
+    private void onLevel1ButtonClick()
+    {
+        SceneManager.LoadScene("1. Grocery Store");
+    }
+
+    private void onExitButtonClick()
+    {
+
+    }
+
+}
