4d8bcb71dd2a201961f9947fdc42622005feb23c
KooNaRi <n.hodu210@gmail.com>
2024-10-25 09:38:11 +0900

    Done 버튼 추가, 씬 이름 변경, 2번 씬에서 Exit 버튼 기능 변경

--- Assets/02_Scripts/InGameUIManager.cs
+++ Assets/02_Scripts/InGameUIManager.cs
@@ -6,19 +6,22 @@ using TMPro;
 public class InGameUIManager : MonoBehaviour
 {
     [SerializeField] private Button exitButton;
+    [SerializeField] private Button doneButton;
 
     void Start()
     {
         exitButton.onClick.AddListener(() => onExitButtonClick());
+        doneButton.onClick.AddListener(() => onDoneButtonClick());
+    }
+
+    private void onDoneButtonClick()//Done 버튼 클릭 시 작동하는 로직 구현 필요
+    {
+
     }
 
     private void onExitButtonClick()
     {
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
+        SceneManager.LoadScene("1. Lobby");
     }
 
 }
