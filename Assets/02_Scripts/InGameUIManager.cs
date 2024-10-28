6418231207037d7eeb7616af5367f80532a0e5b0
KooNaRi <n.hodu210@gmail.com>
2024-10-28 13:01:38 +0900

    Done 버튼, 기능  추가 및 checkout 스크립트 삭제

--- Assets/02_Scripts/InGameUIManager.cs
+++ Assets/02_Scripts/InGameUIManager.cs
@@ -8,15 +8,19 @@ public class InGameUIManager : MonoBehaviour
     [SerializeField] private Button exitButton;
     [SerializeField] private Button doneButton;
 
+    public Cart cart; // 장바구니 객체를 참조
+
     void Start()
     {
         exitButton.onClick.AddListener(() => onExitButtonClick());
         doneButton.onClick.AddListener(() => onDoneButtonClick());
     }
 
-    private void onDoneButtonClick()//Done 버튼 클릭 시 작동하는 로직 구현 필요
+    private void onDoneButtonClick()//Done 버튼 클릭 시 작동하는 로직 구현
     {
-
+        float totalPrice = cart.CalculateTotalPrice();
+        Debug.Log("총 가격: " + totalPrice + "원");
+        cart.ClearCart(); // 결제 후 장바구니를 비움
     }
 
     private void onExitButtonClick()
